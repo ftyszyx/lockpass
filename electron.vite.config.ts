54ff508b617818708a29206880f502e582763048
ftyszyx <whyzi@qq.com>
2024-08-29 22:48:58 +0800

    apk build

---
+++ electron.vite.config.ts
@@ -0,0 +1,27 @@
+import { resolve } from 'path'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: {
+        '@main': resolve('src/main'),
+        '@common': resolve('src/common')
+      }
+    }
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+        '@common': resolve('src/common')
+      }
+    },
+    plugins: [react()]
+  }
+})
